5d9a2dc14bc813ed8f68a5aa5ded4d2ff93e835d
kevin <kev.aar_on@hotmail.es>
2026-09-07 21:49:33 -0600

    Actualizar vista principal CxC

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+
 import { ComponentShowcase } from '../components/ui/ComponentShowcase';
 import { routes } from './routes';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Navigate to="/cxc/documentos/documentos" replace />,
+  },
+  {
+    path: '/ui-kit',
     element: <ComponentShowcase />,
   },
   ...routes,
